d9f4f76a7f2cd9b0b29d5556414c97f65f83bbb3
pasindusgkmp <pasindugunasekara889@gmail.com>
2025-07-19 14:14:15 +0530

    drop down menu done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,18 @@
-import { Moon } from "lucide-react";
+import { LogOut, Moon, Settings, User } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 import { Avatar } from "./ui/avatar";
 import { AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 
 export default function Navbar() {
   return (
@@ -10,15 +20,38 @@ export default function Navbar() {
         {/* {LEFT} */}
         collapsButton
 
-        {/* {RIGHT}dgrtxvgsxtgf*/}
+        {/* {RIGHT}*/}
         
         <div className="flex items-center gap-4">
         <Link href="/">Dashboard</Link>
         <Moon />
-        <Avatar>
+       
+        <DropdownMenu>
+        <DropdownMenuTrigger>
+           <Avatar>
             <AvatarImage src="https://github.com/shadcn.png" />
             <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
+           </Avatar>
+
+        </DropdownMenuTrigger>
+        <DropdownMenuContent sideOffset={10}>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <User className="h-[1.2rem] w-[1.2rem] mr-2"/>
+            profile
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Settings className="h-[1.2rem] w-[1.2rem] mr-2"/>
+            Settings
+          </DropdownMenuItem>
+          <DropdownMenuItem variant="destructive">
+            <LogOut className="h-[1.2rem] w-[1.2rem] mr-2"/>
+            Logout
+          </DropdownMenuItem>
+          {/* <DropdownMenuItem>Subscription</DropdownMenuItem> */}
+        </DropdownMenuContent>
+      </DropdownMenu>
         </div>
     </nav>
   );
